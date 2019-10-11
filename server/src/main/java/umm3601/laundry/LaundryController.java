1e0e058819b9597a9a0777bc21414cba9643190f
wallerli <lsw626313@gmail.com>
2019-10-11 18:58:14 -0500

    Support server-side machine APIs #9 Support server-side room APIs #8

---
+++ server/src/main/java/umm3601/laundry/LaundryController.java
@@ -0,0 +1,57 @@
+package umm3601.laundry;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class LaundryController {
+  private final MongoCollection<Document> roomCollection;
+  private final MongoCollection<Document> machineCollection;
+
+  public LaundryController(MongoDatabase machineDatabase, MongoDatabase roomDatabase) {
+    machineCollection = machineDatabase.getCollection("machines");
+    roomCollection = roomDatabase.getCollection("rooms");
+  }
+
+
+  public Object getRooms() {
+    return serializeIterable(roomCollection.find());
+  }
+
+  public Object getMachines() {
+    return serializeIterable(machineCollection.find());
+  }
+
+  public String getMachinesAtRoom(String room) {
+    Document filterDoc = new Document();
+    filterDoc = filterDoc.append("room_id", room);
+    return serializeIterable(machineCollection.find(filterDoc));
+  }
+
+  private String serializeIterable(Iterable<Document> documents) {
+    return StreamSupport.stream(documents.spliterator(), false)
+      .map(Document::toJson)
+      .collect(Collectors.joining(", ", "[", "]"));
+  }
+
+  public String getMachine(String id) {
+    FindIterable<Document> jsonMachines
+      = machineCollection.find(eq("id", id));
+
+    Iterator<Document> iterator = jsonMachines.iterator();
+    if (iterator.hasNext()) {
+      Document machine = iterator.next();
+      return machine.toJson();
+    } else {
+      // We didn't find the desired machine
+      return null;
+    }
+  }
+}
