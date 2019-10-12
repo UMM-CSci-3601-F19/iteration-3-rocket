a0438ca59da152fa95ed18eead28708127643620
wallerli <lsw626313@gmail.com>
2019-10-11 21:48:11 -0500

    Implement MongoDB room tests #17

--- server/src/test/java/umm3601/mongotest/MongoMachineSpec.java
+++ server/src/test/java/umm3601/mongotest/MongoMachineSpec.java
@@ -1,4 +1,29 @@
 package umm3601.mongotest;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.*;
+import org.bson.Document;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class MongoMachineSpec {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
