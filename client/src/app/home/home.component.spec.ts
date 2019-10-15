e1172506aedf1a049e883a12cdc8d8bb1b1b3162
rowla070 <rowla070@morris.umn.edu>
2019-10-14 21:23:00 -0500

    Fix the client home service error #28 #26

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -1,8 +1,10 @@
+
 import {TestBed, ComponentFixture} from '@angular/core/testing';
 import {HomeComponent} from './home.component';
 import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
 import {CustomModule} from '../custom.module';
+import {HomeService} from './home.service';
 
 describe('Home', () => {
 
@@ -10,13 +12,18 @@ describe('Home', () => {
   let fixture: ComponentFixture<HomeComponent>;
   let de: DebugElement;
   let df: DebugElement;
+  let dg: DebugElement;
+  let dh: DebugElement;
   let el: HTMLElement;
   let fl: HTMLElement;
+  let gl: HTMLElement;
+  let hl: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [CustomModule],
       declarations: [HomeComponent], // declare the test component
+      providers: [{provide: HomeService}]
     });
 
     fixture = TestBed.createComponent(HomeComponent);
@@ -25,18 +32,32 @@ describe('Home', () => {
 
     // query for the link (<a> tag) by CSS element selector
     de = fixture.debugElement.query(By.css('#home-rooms-card'));
-    df = fixture.debugElement.query(By.css('#home-machines-card'));
+    df = fixture.debugElement.query(By.css('#home-machines-card-washer'));
+    dg = fixture.debugElement.query(By.css('#home-machines-card-dryer'));
+    dh = fixture.debugElement.query(By.css('#home-machines-card-broken'));
     el = de.nativeElement;
     fl = df.nativeElement;
+    gl = dg.nativeElement;
+    hl = dh.nativeElement;
   });
 
-  // it('displays a text of rooms', () => {
-  //   fixture.detectChanges();
-  //   expect(el.textContent).toContain("Rooms!");
-  // });
+  it('displays a text of rooms', () => {
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Select a Laundry Room Here');
+  });
 
-  // it('displays a text of machines', () => {
-  //   fixture.detectChanges();
-  //   expect(fl.textContent).toContain("Machines!");
-  // });
+  it('displays a text of washers', () => {
+    fixture.detectChanges();
+    expect(fl.textContent).toContain('Washers available within all rooms');
+  });
+
+  it('displays a text of dryers', () => {
+    fixture.detectChanges();
+    expect(gl.textContent).toContain('Dryers available within all rooms');
+  });
+
+  it('displays a text of broken machines', () => {
+    fixture.detectChanges();
+    expect(hl.textContent).toContain('Unavailable machines within all rooms');
+  });
 });
