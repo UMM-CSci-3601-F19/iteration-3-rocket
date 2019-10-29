67e3a0d72e3056ebddadcd9a686fc8b07b078699
wallerli <lsw626313@gmail.com>
2019-10-29 11:31:38 -0500

    Change all room button position #12

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -97,7 +97,7 @@ public class Server {
     get("api/rooms/:room/machines", laundryRequestHandler::getRoomMachines);
     get("api/machines", laundryRequestHandler::getMachines);
     get("api/machines/:machine", laundryRequestHandler::getMachineJSON);
-//  get("api/change_machine_status/:machine_id/:status", laundryRequestHandler::changeMachineStatus);
+//    get("api/change_machine_status/:machine_id/:status", laundryRequestHandler::changeMachineStatus);
 
     // Returns the history of the up-coming 24h of last week
     get("api/history/rooms", historyRequestHandler::getHistory);
