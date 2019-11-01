b2df436954c021999751008e58db0f531d14bda4
wallerli <lsw626313@gmail.com>
2019-11-01 16:09:29 -0500

    Decrease scroll delay #12

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -143,7 +143,7 @@ export class HomeComponent implements OnInit {
   }
 
   scroll(id: string) {
-    this.delay(200).then(() => document.getElementById(id).scrollIntoView());
+    this.delay(150).then(() => document.getElementById(id).scrollIntoView());
   }
 
   hideSelector() {
