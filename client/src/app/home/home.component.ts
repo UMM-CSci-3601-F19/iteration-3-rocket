84c507f30944db5b13fd4c0cf11e587d1df5f5e2
wallerli <lsw626313@gmail.com>
2019-11-08 01:19:40 -0600

    Update local machine seed data Improve name translation function to the client

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -515,10 +515,9 @@ export class HomeComponent implements OnInit {
   }
 
   translateMachineName(name: string): string {
-    name = name.replace('-', ' ');
-    name = name.replace('-', ' ');
-    // return name.substr(0, i).toUpperCase() + ' ' + name.substr(i + 1, j).toUpperCase()
-    //   + ' ' + name.substr(j + 1).toUpperCase();
+    while (name.indexOf('-') !== -1) {
+      name = name.replace('-', ' ');
+    }
     return name;
   }
 
