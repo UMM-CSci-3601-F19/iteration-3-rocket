415d4315919daff0947a6099ad2a8baec2a51a35
wallerli <lsw626313@gmail.com>
2019-11-23 16:46:52 -0600

    Update fake machine and room data with new interface #21 #5

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -10,11 +10,11 @@ import {History} from './history';
 import {Observable} from 'rxjs';
 import {CookieService} from 'ngx-cookie-service';
 
-import {Subscription} from "./subscription";
-import {NgForm} from "@angular/forms";
+import {Subscription} from './subscription';
+import {NgForm} from '@angular/forms';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import {SubscriptionDialog} from './home.component';
-import {AddUserComponent} from "../users/add-user.component";
+import {AddUserComponent} from '../users/add-user.component';
 
 describe('Home page', () => {
 
@@ -61,6 +61,7 @@ describe('Home page', () => {
           },
           remainingTime: -1,
           vacantTime: 10,
+          isSubscribed: undefined
         }, {
           id: 'id_2',
           name: '',
@@ -74,6 +75,7 @@ describe('Home page', () => {
           },
           remainingTime: 10,
           vacantTime: -1,
+          isSubscribed: undefined
         },
       ]),
       getRooms: () => Observable.of([
@@ -699,9 +701,10 @@ describe('Home page', () => {
     expect(component.filteredMachines.length).toEqual(1);
   });
 
-  describe('Add subscription cdialog', () => {
+  describe('Add subscription dialog', () => {
 
     let subscriptionDialog: SubscriptionDialog;
+    // tslint:disable-next-line:no-shadowed-variable
     let fixture: ComponentFixture<SubscriptionDialog>;
 
     let calledClose: boolean;
@@ -731,9 +734,10 @@ describe('Home page', () => {
     });
 
     it('should not allow to subscribe with an invalid form of email'), async(() => {
-      let fixture = TestBed.createComponent(SubscriptionDialog);
-      let debug = fixture.debugElement;
-      let input = debug.query(By.css('[name=email]'));
+      // tslint:disable-next-line:no-shadowed-variable
+      const fixture = TestBed.createComponent(SubscriptionDialog);
+      const debug = fixture.debugElement;
+      const input = debug.query(By.css('[name=email]'));
 
       fixture.detectChanges();
       fixture.whenStable().then(() => {
@@ -741,8 +745,8 @@ describe('Home page', () => {
         dispatchEvent(input.nativeElement);
         fixture.detectChanges();
 
-        let form: NgForm = debug.children[0].injector.get(NgForm);
-        let control = form.control.get('email');
+        const form: NgForm = debug.children[0].injector.get(NgForm);
+        const control = form.control.get('email');
         expect(control.hasError('notPeeskillet')).toBe(true);
         expect(form.control.valid).toEqual(false);
         expect(form.control.hasError('notPeeskillet', ['email'])).toEqual(true);
@@ -756,5 +760,5 @@ describe('Home page', () => {
         expect(form.control.hasError('notPeeskillet', ['email'])).toEqual(false);
       });
     });
-  })
+  });
 });
