ed8d1b89d12b2ed90a8d5bb7ac67810b10852c42
wallerli <lsw626313@gmail.com>
2019-11-23 16:41:15 -0600

    Update machine and subscription interface for machine subscriptions #5

--- client/src/app/users/add-user.component.spec.ts
+++ client/src/app/users/add-user.component.spec.ts
@@ -4,8 +4,8 @@ import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 import {AddUserComponent} from './add-user.component';
 import {CustomModule} from '../custom.module';
-import {By} from "@angular/platform-browser";
-import {NgForm} from "@angular/forms";
+import {By} from '@angular/platform-browser';
+import {NgForm} from '@angular/forms';
 
 describe('Add user component', () => {
 
@@ -41,10 +41,11 @@ describe('Add user component', () => {
   // https://stackoverflow.com/questions/52046741/angular-testbed-query-by-css-find-the-pseudo-element
   // https://angular.io/guide/form-validation
   // https://github.com/angular/angular/blob/7.2.2/packages/forms/src/validators.ts#L136-L157
-  it('should not allow a name to contain a symbol'), async(() => {
-    let fixture = TestBed.createComponent(AddUserComponent);
-    let debug = fixture.debugElement;
-    let input = debug.query(By.css('[name=email]'));
+  it('should not allow a name to contain a symbol', async( () => {
+    // tslint:disable-next-line:no-shadowed-variable
+    const fixture = TestBed.createComponent(AddUserComponent);
+    const debug = fixture.debugElement;
+    const input = debug.query(By.css('[name=email]'));
 
     fixture.detectChanges();
     fixture.whenStable().then(() => {
@@ -52,8 +53,8 @@ describe('Add user component', () => {
       dispatchEvent(input.nativeElement);
       fixture.detectChanges();
 
-      let form: NgForm = debug.children[0].injector.get(NgForm);
-      let control = form.control.get('email');
+      const form: NgForm = debug.children[0].injector.get(NgForm);
+      const control = form.control.get('email');
       expect(control.hasError('notPeeskillet')).toBe(true);
       expect(form.control.valid).toEqual(false);
       expect(form.control.hasError('notPeeskillet', ['email'])).toEqual(true);
@@ -66,5 +67,6 @@ describe('Add user component', () => {
       expect(form.control.valid).toEqual(true);
       expect(form.control.hasError('notPeeskillet', ['email'])).toEqual(false);
     });
-  });
+  })
+  );
 });
