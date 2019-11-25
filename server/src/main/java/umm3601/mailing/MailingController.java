b1452cca865abc3916f4581ece487e495b9d7ee8
wallerli <lsw626313@gmail.com>
2019-11-24 20:48:11 -0600

    Finalize the send key Remove an unused comment

--- server/src/main/java/umm3601/mailing/MailingController.java
+++ server/src/main/java/umm3601/mailing/MailingController.java
@@ -85,8 +85,8 @@ public class MailingController {
   }
 
   private int send(Mail mail) throws IOException {
-    String key = "put_your_key_here";
 //  String key = System.getenv("SENDGRID_API_KEY");
+    final String key = "put_your_key_here";
     SendGrid sg = new SendGrid(key);
     Request request = new Request();
     request.setMethod(Method.POST);
